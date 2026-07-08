da5a97ebdaf10c90d7eebc586b76ccd8605de793
tako0614 <shoutatomiyama0614@gmail.com>
2026-07-08 22:33:34 +0000

    takos-storage: accept generic app env inputs

--- outputs.tf
+++ outputs.tf
@@ -95,9 +95,12 @@ output "app_deployment" {
       },
     ]
 
-    env = {
-      APP_URL = local.launch_url != null ? local.launch_url : ""
-    }
+    env = merge(
+      local.extra_worker_env,
+      {
+        APP_URL = local.launch_url != null ? local.launch_url : ""
+      },
+    )
   }
 }
 
